a1f3f7af494760b1a8817ef0077e2a6affa816d2
datagonerogue <botanicaljanuary320@slmail.me>
2024-03-06 14:38:51 +0100

    Added Detect function

--- popup.js
+++ popup.js
@@ -12,6 +12,48 @@ document.addEventListener("DOMContentLoaded", function () {
 
   loadTranslationHistory();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const detectButton = document.getElementById("detectButton");
+  const inputText = document.getElementById("inputText");
+  const detectedLanguageDiv = document.getElementById("detectedLanguage");
+
+  detectButton.addEventListener("click", function () {
+    const textToDetect = inputText.value;
+    detectLanguage(textToDetect);
+  });
+});
+
+async function detectLanguage(text) {
+  const formdata = new FormData();
+  formdata.append("key", "ea4424ca1beb145f3fffc22864f8c818");
+  formdata.append("txt", text);
+
+  const requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await fetch(
+      "https://api.meaningcloud.com/lang-4.0/identification",
+      requestOptions
+    );
+    if (response.ok) {
+      const data = await response.json();
+      const detectedLanguage = data.language_list[0].name;
+      const detectedLanguageDiv = document.getElementById("detectedLanguage");
+      detectedLanguageDiv.textContent =
+        "Detected Language: " + detectedLanguage;
+    } else {
+      throw new Error("Failed to detect language");
+    }
+  } catch (error) {
+    console.error("Error detecting language:", error);
+  }
+}
+
 async function translateText(text, targetLanguage) {
   const subscriptionKey = "f41ae025a1e94b4689d8f2dae5e4c635";
   const endpoint = "https://api.cognitive.microsofttranslator.com";
